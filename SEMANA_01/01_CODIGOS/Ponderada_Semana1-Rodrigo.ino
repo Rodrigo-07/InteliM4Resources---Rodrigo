c6acd0935d65c2f4d4abb17f1f23f7544bccc185
Rodrigo Sales <freirerodrigosales@gmail.com>
2023-10-22 19:49:59 -0300

    Ponderada Semana 1

--- SEMANA_01/01_CODIGOS/Ponderada_Semana1-Rodrigo.ino
+++ SEMANA_01/01_CODIGOS/Ponderada_Semana1-Rodrigo.ino
@@ -13,3 +13,5 @@ void loop()
   
   delay(1000); // Delay de 1 segundo
 }
+
+// Link projeto no Tinkercad: https://www.tinkercad.com/things/0s5bXi9yGDB-grand-rottis-luulia/editel?sharecode=Cpc9ZV4_SyIUcAp7fP12eb7RmqhK7JJVvuuSc2W2aUw
